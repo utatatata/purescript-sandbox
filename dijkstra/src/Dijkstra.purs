95ed722b5c8209da6e427bebdf1603b546acc330
utakuma <i@utakuma.info>
2019-06-11 19:32:03 +0900

    format and fix files

--- dijkstra/src/Dijkstra.purs
+++ dijkstra/src/Dijkstra.purs
@@ -1,4 +1,4 @@
-module Dijkstra  where
+module Dijkstra where
 
 import Prelude
 -- import Data.Foldable (class Foldable)
@@ -18,7 +18,6 @@ import Data.Traversable
 import Data.Tuple
 import Data.Unfoldable hiding (range, fromMaybe)
 
-
 type Vertex
   = Int
 
@@ -35,51 +34,47 @@ type Depth
   = Int
 
 getVerteces :: Graph -> List Vertex
-getVerteces graph =
-  graph # concatMap (\(Tuple v1 v2) -> v1:v2:Nil)
+getVerteces graph = graph # concatMap (\(Tuple v1 v2) -> v1 : v2 : Nil)
 
 getCurrentVerteces :: List Path -> List Vertex
 getCurrentVerteces paths =
-  paths # concatMap
-    (\path ->
-      case head path of
-        Just v -> v:Nil
-        Nothing -> Nil
-    )
+  paths
+    # concatMap
+        ( \path -> case head path of
+          Just v -> v : Nil
+          Nothing -> Nil
+        )
 
 getNext :: Graph -> Vertex -> Maybe (List Vertex)
-getNext g v =
-  case g # filter ((eq v) <<< fst)
-         # map snd of
-    Nil -> Nothing
-    vs -> Just vs
-  
+getNext g v = case g # filter ((eq v) <<< fst)
+    # map snd of
+  Nil -> Nothing
+  vs -> Just vs
+
 getOneStepVerteces :: Graph -> Path -> Maybe (List Vertex)
 getOneStepVerteces _ Nil = Nothing
-getOneStepVerteces g (Cons current _) =
-  getNext g current
+
+getOneStepVerteces g (Cons current _) = getNext g current
 
 getOneStepPaths :: Graph -> Path -> Maybe (List Path)
 getOneStepPaths _ Nil = Nothing
+
 getOneStepPaths graph path@(Cons currentVertex _) =
   getNext graph currentVertex
     <#> filter (flip notElem $ path)
-    <#> map (\v -> v:path)
+    <#> map (\v -> v : path)
 
 getNextPaths :: Graph -> List Path -> Maybe (List Path)
-getNextPaths graph paths =
-  case paths # concatMap (fromMaybe Nil <<< (getOneStepPaths graph)) of
-    Nil -> Nothing
-    paths -> Just paths
-
+getNextPaths graph paths = case paths # concatMap (fromMaybe Nil <<< (getOneStepPaths graph)) of
+  Nil -> Nothing
+  paths -> Just paths
 
 getLimitedPaths :: Depth -> Graph -> Vertex -> Maybe (List Path)
-getLimitedPaths depth graph start =
-  applyN ((=<<) (getNextPaths graph)) depth (Just ((start:Nil):Nil))
-
+getLimitedPaths depth graph start = applyN ((=<<) (getNextPaths graph)) depth (Just ((start : Nil) : Nil))
 
 reached :: Vertex -> Path -> Boolean
 reached _ Nil = false
+
 reached goal (Cons current _) = current == goal
 
 data FailedToSearch
@@ -91,46 +86,35 @@ instance showFailedToSearch :: Show FailedToSearch where
   show Unreachable = "Unreachable"
 
 depthLimitedSearch :: Depth -> Graph -> Vertex -> Vertex -> Either FailedToSearch Path
-depthLimitedSearch depth graph start goal =
-  go depth ((start:Nil):Nil)
+depthLimitedSearch depth graph start goal = go depth ((start : Nil) : Nil)
   where
-    go depth currentDepthPaths =
-      case currentDepthPaths # find (reached goal) of
-        Just path -> Right path
-        Nothing ->
-          if depth == 0
-          then Left Unreached
-          else case getNextPaths graph currentDepthPaths of
-            Nothing -> Left Unreachable
-            Just paths -> go (depth - 1) paths
+  go depth currentDepthPaths = case currentDepthPaths # find (reached goal) of
+    Just path -> Right path
+    Nothing -> if depth == 0
+      then Left Unreached
+      else case getNextPaths graph currentDepthPaths of
+        Nothing -> Left Unreachable
+        Just paths -> go (depth - 1) paths
 
 iddfs :: Graph -> Vertex -> Vertex -> Maybe Path
-iddfs graph start goal =
-  go 0
+iddfs graph start goal = go 0
   where
-    go depth =
-      case depthLimitedSearch depth graph start goal of
-        Right path -> Just path
-        Left Unreachable -> Nothing
-        Left Unreached -> go (depth + 1)
+  go depth = case depthLimitedSearch depth graph start goal of
+    Right path -> Just path
+    Left Unreachable -> Nothing
+    Left Unreached -> go (depth + 1)
 
 dijkstra :: Graph -> Vertex -> Vertex -> Maybe Path
-dijkstra graph start goal =
-  go (getVerteces graph) ((start:Nil):Nil)
+dijkstra graph start goal = go (getVerteces graph) ((start : Nil) : Nil)
   where
-    go :: List Vertex -> List Path -> Maybe Path
-    go unsearched currentPaths =
-      case currentPaths # find (reached goal) of
-        Just path -> Just path
-        Nothing ->
-          if null unsearched
-          then Nothing
-          else case getNextPaths graph currentPaths of
-            Nothing -> Nothing
-            Just paths -> go
-              (union unsearched
-                (getCurrentVerteces currentPaths)
-              )
-              paths
-
-
+  go :: List Vertex -> List Path -> Maybe Path
+  go unsearched currentPaths = case currentPaths # find (reached goal) of
+    Just path -> Just path
+    Nothing -> if null unsearched
+      then Nothing
+      else case getNextPaths graph currentPaths of
+        Nothing -> Nothing
+        Just paths ->
+          go
+            ( union unsearched (getCurrentVerteces currentPaths)
+            ) paths
--- dijkstra/src/Main.purs
+++ dijkstra/src/Main.purs
@@ -11,23 +11,30 @@ import Dijkstra
 main :: Effect Unit
 main =
   let
-    graph1 = fromFoldable
-      [ (Tuple 0 1), (Tuple 0 2), (Tuple 0 3)
-      , (Tuple 1 4), (Tuple 2 4), (Tuple 3 5)
-      , (Tuple 4 5), (Tuple 3 5), (Tuple 8 9)
-      ]
-  in do
-    log $ "Graph1: " <>  (show graph1)
-    log "Limited depth search:"
-    log $ "a path from 0 to 4, depth limit is 2: " <> (show (depthLimitedSearch 2 graph1 0 4))
-    log $ "a path from 0 to 4, depth limit is 1: " <> (show (depthLimitedSearch 1 graph1 0 4))
-    log $ "a path from 0 to 8, depth limit is 10: " <> (show (depthLimitedSearch 10 graph1 0 8))
-    log "IDDFS:"
-    log $ "a path from 0 to 4: " <> (show (iddfs graph1 0 4))
-    log $ "a path from 0 to 5: " <> (show (iddfs graph1 0 5))
-    log $ "a path from 0 to 8: " <> (show (iddfs graph1 0 8))
-    log "Dijkstra:"
-    log $ "a path from 0 to 4: " <> (show (dijkstra graph1 0 4))
-    log $ "a path from 0 to 5: " <> (show (dijkstra graph1 0 5))
-    log $ "a path from 0 to 8: " <> (show (dijkstra graph1 0 8))
-
+    graph1 =
+      fromFoldable
+        [ (Tuple 0 1)
+        , (Tuple 0 2)
+        , (Tuple 0 3)
+        , (Tuple 1 4)
+        , (Tuple 2 4)
+        , (Tuple 3 5)
+        , (Tuple 4 5)
+        , (Tuple 3 5)
+        , (Tuple 8 9)
+        ]
+  in
+    do
+      log $ "Graph1: " <> (show graph1)
+      log "Limited depth search:"
+      log $ "a path from 0 to 4, depth limit is 2: " <> (show (depthLimitedSearch 2 graph1 0 4))
+      log $ "a path from 0 to 4, depth limit is 1: " <> (show (depthLimitedSearch 1 graph1 0 4))
+      log $ "a path from 0 to 8, depth limit is 10: " <> (show (depthLimitedSearch 10 graph1 0 8))
+      log "IDDFS:"
+      log $ "a path from 0 to 4: " <> (show (iddfs graph1 0 4))
+      log $ "a path from 0 to 5: " <> (show (iddfs graph1 0 5))
+      log $ "a path from 0 to 8: " <> (show (iddfs graph1 0 8))
+      log "Dijkstra:"
+      log $ "a path from 0 to 4: " <> (show (dijkstra graph1 0 4))
+      log $ "a path from 0 to 5: " <> (show (dijkstra graph1 0 5))
+      log $ "a path from 0 to 8: " <> (show (dijkstra graph1 0 8))
