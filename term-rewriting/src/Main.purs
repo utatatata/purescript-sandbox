69d2d1c9ece8f0c1a56991b6ddf7093c500c1f88
utakuma <i@utakuma.info>
2019-06-19 14:51:05 +0900

    [term-rewriting] modify to use Maybe

--- term-rewriting/src/Main.purs
+++ term-rewriting/src/Main.purs
@@ -3,12 +3,16 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log, logShow)
+import Data.Show (show)
 import Trs
 
 main :: Effect Unit
 main =
   let
     term1 = (Concat (Cons 0 (Cons 1 (Cons 2 Nil))) (Cons 3 (Cons 4 Nil)))
+    term2 = (Snoc (Cons 0 (Cons 1 (Cons 2 Nil))) 3)
   in do
   logShow term1
-  logShow $ rewrite term1
+  log $ "rewrite: " <> (show $ rewrite term1)
+  logShow term2
+  log $ "rewrite: " <> (show $ rewrite term2)
--- term-rewriting/src/Trs.purs
+++ term-rewriting/src/Trs.purs
@@ -2,20 +2,25 @@ module Trs where
 
 import Prelude
 import Data.List
+import Data.Maybe
 
 data Term a
     = Nil
     | Cons a (Term a)
     | Concat (Term a) (Term a)
+    | Snoc (Term a) a
 
 derive instance eqTermInt :: Eq (Term Int)
 instance showTermInt :: Show (Term Int) where
     show Nil = "Nil"
     show (Cons x y) = "(Cons " <> show x <> " " <> show y <> ")"
     show (Concat x y) = "(Concat " <> show x <> " " <> show y <> ")"
+    show (Snoc x y) = "(Snoc " <> show x <> " " <> show y <> ")"
 
-rewrite :: forall a. Term a -> Term a
-rewrite (Cons x y) = Cons x $ rewrite y
+rewrite :: forall a. Term a -> Maybe (Term a)
+rewrite (Cons x y) = (Cons x) <$> (rewrite y)
 rewrite (Concat Nil term) = rewrite term
 rewrite (Concat (Cons head tail) term) =  rewrite $ Cons head (Concat tail term)
-rewrite term = term
+rewrite (Snoc Nil a) = rewrite $ Cons a Nil
+rewrite (Snoc (Cons head tail) a) = rewrite $ Cons head (Snoc tail a)
+rewrite term = Just term
