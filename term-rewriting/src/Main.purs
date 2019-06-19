d75bcc6bea33eb118f67fb82bdd2e8e6104df1bf
utakuma <i@utakuma.info>
2019-06-20 00:57:56 +0900

    [term-rewriting] format and fix files

--- term-rewriting/src/Main.purs
+++ term-rewriting/src/Main.purs
@@ -9,9 +9,11 @@ main :: Effect Unit
 main =
   let
     term1 = (Concat (Cons 0 (Cons 1 (Cons 2 Nil))) (Cons 3 (Cons 4 Nil)))
+
     term2 = (Snoc (Cons 0 (Cons 1 (Cons 2 Nil))) 3)
-  in do
-  logShow term1
-  log $ "rewrite: " <> (show $ rewrite term1)
-  logShow term2
-  log $ "rewrite: " <> (show $ rewrite term2)
+  in
+    do
+      logShow term1
+      log $ "rewrite: " <> (show $ rewrite term1)
+      logShow term2
+      log $ "rewrite: " <> (show $ rewrite term2)
--- term-rewriting/src/Trs.purs
+++ term-rewriting/src/Trs.purs
@@ -4,28 +4,32 @@ import Prelude (class Show, class Eq, show, ($), (<@>), (<$>), (<>))
 import Data.Maybe (Maybe(..))
 
 data Term a
-    = Nil
-    | Cons a (Term a)
-    | Concat (Term a) (Term a)
-    | Snoc (Term a) a
+  = Nil
+  | Cons a (Term a)
+  | Concat (Term a) (Term a)
+  | Snoc (Term a) a
 
 derive instance eqTermInt :: Eq (Term Int)
+
 instance showTermInt :: Show (Term Int) where
-    show Nil = "Nil"
-    show (Cons x y) = "(Cons " <> show x <> " " <> show y <> ")"
-    show (Concat x y) = "(Concat " <> show x <> " " <> show y <> ")"
-    show (Snoc x y) = "(Snoc " <> show x <> " " <> show y <> ")"
+  show Nil = "Nil"
+  show (Cons x y) = "(Cons " <> show x <> " " <> show y <> ")"
+  show (Concat x y) = "(Concat " <> show x <> " " <> show y <> ")"
+  show (Snoc x y) = "(Snoc " <> show x <> " " <> show y <> ")"
 
 rewrite :: forall a. Term a -> Maybe (Term a)
 rewrite Nil = Just Nil
+
 rewrite (Cons head tail) = (Cons head) <$> (rewrite tail)
 
 rewrite (Concat Nil term) = rewrite term
-rewrite (Concat (Cons head tail) term) =  rewrite $ Cons head (Concat tail term)
+
+rewrite (Concat (Cons head tail) term) = rewrite $ Cons head (Concat tail term)
+
 rewrite (Concat term1 term2) = Concat <$> (rewrite term1) <@> term2
 
 rewrite (Snoc Nil last) = rewrite $ Cons last Nil
+
 rewrite (Snoc (Cons head tail) last) = rewrite $ Cons head (Snoc tail last)
+
 rewrite (Snoc term last) = Snoc <$> (rewrite term) <@> last
-
-
